0b497ee47a1f0591d5e2a6b1ee59690ebcb3bb83
Yessica Brevis <y.brevis@gmail.com>
2026-07-07 06:39:13 -0400

    Main.java

--- LanquihueTourApp/src/ui/Main.java
+++ LanquihueTourApp/src/ui/Main.java
@@ -5,16 +5,21 @@ import data.GestorServicios;
 import java.util.ArrayList;
 import model.ServicioTuristico;
 
+// Clase principal que ejecuta el sistema Llanquihue Tour.
 public class Main {
 
     public static void main(String[] args) {
-
+ 
+        // Crea una instancia del gestor de servicios.
         GestorServicios gestor = new GestorServicios();
 
+        // Genera la colección de servicios turísticos.
         ArrayList<ServicioTuristico> servicios = gestor.generarServicios();
 
+        // Muestra el encabezado del listado.
         System.out.println("=== LISTA DE SERVICIOS TURISTICOS ===");
 
+        // Recorre la colección de servicios utilizando polimorfismo.
         for (ServicioTuristico servicioTuristico : servicios) {
 
             System.out.println("-----------------------------");
